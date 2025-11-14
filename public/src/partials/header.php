d1172459602d56f093276af2be85ea52a5c87198
bernatzx <andikasukardi1@gmail.com>
2025-11-14 11:07:54 +0900

    init partials and admin dashboard page

---
+++ public/src/partials/header.php
@@ -0,0 +1,33 @@
+<?php require_once __DIR__ . "/../../../app/init.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Penjualan DISTRO | Sistem Informasi Penjualan</title>
+  <script src="<?= base('public/assets/js/all.min.js') ?>" defer></script>
+  <script src="<?= base('public/assets/js/tailwindcss.js') ?>"></script>
+  <style>
+    body {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+    }
+  </style>
+</head>
+
+<body>
+
+  <div class="bg-gray-50 h-screen p-5">
+    <div class="bg-white shadow-md border flex py-2 px-5 items-center justify-between font-medium">
+      <div class="text-2xl">Selamat Datang di DM Store</div>
+      <div
+        class="text-sm flex items-center gap-2 bg-red-500 text-gray-50 py-1 px-3 rounded-md border border-red-600 shadow-md cursor-pointer hover:opacity-70">
+        <div>
+          <i class="fas fa-right-from-bracket fa-fw"></i>
+        </div>
+        <div>Logout</div>
+      </div>
+    </div>
+    <div class="w-full max-w-4xl m-auto pt-[50px]">
--- public/src/views/dashboard/index.php
+++ public/src/views/dashboard/index.php
@@ -1,1 +1,52 @@
-dashboard
+<?php require_once __DIR__ . '/../../partials/header.php' ?>
+
+<div class="capitalize font-medium text-xl mb-4">dashboard admin</div>
+
+<!-- cards -->
+<div class="grid grid-cols-3 gap-5 mb-[50px]">
+  <div class="flex items-center bg-white py-3 px-5 font-medium shadow-md border rounded-md justify-between">
+    <div>
+      <div>Total Pesanan</div>
+      <div class="text-gray-400">10</div>
+    </div>
+    <div class="text-3xl text-gray-400">
+      <i class="fas fa-dolly fa-fw"></i>
+    </div>
+  </div>
+  <div class="flex items-center bg-white py-3 px-5 font-medium shadow-md border rounded-md justify-between">
+    <div>
+      <div>Jumlah Produk</div>
+      <div class="text-gray-400">10</div>
+    </div>
+    <div class="text-3xl text-gray-400">
+      <i class="fas fa-box-open fa-fw"></i>
+    </div>
+  </div>
+  <div class="flex items-center bg-white py-3 px-5 font-medium shadow-md border rounded-md justify-between">
+    <div>
+      <div>Pengguna Terdaftar</div>
+      <div class="text-gray-400">10</div>
+    </div>
+    <div class="text-3xl text-gray-400">
+      <i class="fas fa-user-group fa-fw"></i>
+    </div>
+  </div>
+</div>
+
+<div class="capitalize font-medium text-xl mb-4">navigasi cepat</div>
+<div class="grid grid-cols-2 gap-5 text-sm">
+  <div
+    class="bg-gray-800 px-5 py-3 text-gray-100 text-center font-medium rounded-md shadow-md cursor-pointer hover:opacity-70">
+    Kelola Produk</div>
+  <div
+    class="bg-gray-800 px-5 py-3 text-gray-100 text-center font-medium rounded-md shadow-md cursor-pointer hover:opacity-70">
+    Kelola Pesanan</div>
+  <div
+    class="bg-gray-800 px-5 py-3 text-gray-100 text-center font-medium rounded-md shadow-md cursor-pointer hover:opacity-70">
+    Kelola Pengguna</div>
+  <div
+    class="bg-gray-800 px-5 py-3 text-gray-100 text-center font-medium rounded-md shadow-md cursor-pointer hover:opacity-70">
+    Lihat Laporan</div>
+</div>
+
+<?php require_once __DIR__ . '/../../partials/footer.php' ?>
