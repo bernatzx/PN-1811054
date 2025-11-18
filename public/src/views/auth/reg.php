ee5e268481bcc4d69fbb2fe68e26b43150454ec5
bernatzx <andikasukardi1@gmail.com>
2025-11-18 15:39:26 +0900

    add ui reg.php

--- public/src/views/auth/reg.php
+++ public/src/views/auth/reg.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login | Sistem Informasi Penjualan</title>
+  <title>Registrasi | Sistem Informasi Penjualan</title>
   <script src="<?= base('public/assets/js/all.min.js') ?>" defer></script>
   <script src="<?= base('public/assets/js/tailwindcss.js') ?>"></script>
   <style>
@@ -22,11 +22,16 @@
   <div class="h-screen flex justify-center items-center">
     <div class="max-w-md bg-gray-50 w-full shadow-lg border-2 p-5 rounded-lg">
       <div class="text-center mb-5">
-        <span class="text-4xl text-gray-800 font-bold">DM STORE</span>
+        <span class="text-4xl text-gray-800 font-bold">Registrasi DM STORE</span>
         <br>
-        <span class="font-semibold text-gray-500">Sistem Informasi Penjualan Distro</span>
+        <span class="font-semibold text-gray-500">Buat akun baru untuk mulai berbelanja</span>
       </div>
       <form>
+        <div class="mb-4">
+          <label class="block mb-2 font-semibold">Nama Lengkap</label>
+          <input type="text" class="block border w-full p-2 text-sm rounded-md border-gray-500 outline-none"
+            placeholder="Masukkan nama lengkap" required>
+        </div>
         <div class="mb-4">
           <label class="block mb-2 font-semibold">Email</label>
           <input type="text" class="block border w-full p-2 text-sm rounded-md border-gray-500 outline-none"
@@ -37,14 +42,19 @@
           <input type="password" class="block border w-full p-2 text-sm rounded-md border-gray-500 outline-none"
             placeholder="Masukkan password" required>
         </div>
+        <div class="mb-4">
+          <label class="block mb-2 font-semibold">Konfirmasi Password</label>
+          <input type="password" class="block border w-full p-2 text-sm rounded-md border-gray-500 outline-none"
+            placeholder="Masukkan konfirmasi password" required>
+        </div>
         <button class="bg-gray-800 rounded-md w-full p-2 text-white font-semibold mb-4" type="submit">Login</button>
       </form>
       <div class="font-semibold text-center">
         <span class="text-gray-500">
-          Belum punya akun?
+          Suda punya akun?
         </span>
-        <a href="reg.php" class="text-blue-500">
-          Daftar
+        <a href="login.php" class="text-blue-500">
+          Login
         </a>
       </div>
     </div>
