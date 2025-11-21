83eb2f5cf97e2cac3783f951d1850be6e07ab3f3
bernatzx <andikasukardi1@gmail.com>
2025-11-21 12:42:15 +0900

    show product in userdash and remove footers ui

--- public/src/components/user-dash.php
+++ public/src/components/user-dash.php
@@ -34,67 +34,62 @@
 </div>
 
 <div class="capitalize font-medium text-xl mb-4">semua produk</div>
-<div class="grid grid-cols-4 gap-4 font-medium">
+<div class="grid grid-cols-4 gap-4 font-medium" id="grid-body">
 
   <!-- CARD -->
-  <div class="bg-white border rounded-md shadow-md p-3 space-y-2">
-    <div class="border-2 border-gray-300 rounded-tl-3xl rounded-br-3xl overflow-hidden">
-      <img src="<?= base('/public/uploads/kaos.png') ?>" alt="kaos">
-    </div>
-    <div>
-      <span class="text-gray-800">Kaos</span>
-      <br>
-      <span class="text-gray-500">Rp.100.000</span>
-    </div>
-    <div
-      class="bg-gray-800 flex gap-2 justify-center items-center rounded-md shadow-md hover:shadow-lg text-gray-100 text-center px-3 py-1">
+  <template id="product-card">
+    <div class="bg-white border rounded-md shadow-md p-3 space-y-2">
+      <div class="border-2 border-gray-300 flex items-center h-[200px] rounded-tl-3xl rounded-br-3xl overflow-hidden">
+        <img class="w-full h-full object-cover object-center">
+      </div>
       <div>
-        <i class="fas fa-fw fa-cart-plus"></i>
+        <span class="nama text-gray-800"></span>
+        <br>
+        <span class="harga text-gray-500"></span>
       </div>
-      <div class="text-xs">
-        Tambah ke keranjang
-      </div>
-    </div>
-  </div>
-
-  <div class="bg-white border rounded-md shadow-md p-3 space-y-2">
-    <div class="border-2 border-gray-300 rounded-tl-3xl rounded-br-3xl overflow-hidden">
-      <img src="<?= base('/public/uploads/celana.png') ?>" alt="celana">
-    </div>
-    <div>
-      <span class="text-gray-800">Celana</span>
-      <br>
-      <span class="text-gray-500">Rp.300.000</span>
-    </div>
-    <div
-      class="bg-gray-800 flex gap-2 justify-center items-center rounded-md shadow-md hover:shadow-lg text-gray-100 text-center px-3 py-1">
-      <div>
-        <i class="fas fa-fw fa-cart-plus"></i>
-      </div>
-      <div class="text-xs">
-        Tambah ke keranjang
+      <div
+        class="bg-gray-800 flex gap-2 justify-center items-center rounded-md shadow-md hover:shadow-lg text-gray-100 text-center px-3 py-1">
+        <div>
+          <i class="fas fa-fw fa-cart-plus"></i>
+        </div>
+        <div class="text-xs">
+          Tambah ke keranjang
+        </div>
       </div>
     </div>
-  </div>
-
-  <div class="bg-white border rounded-md shadow-md p-3 space-y-2">
-    <div class="border-2 border-gray-300 rounded-tl-3xl rounded-br-3xl overflow-hidden">
-      <img src="<?= base('/public/uploads/hoodie.png') ?>" alt="hoodie">
-    </div>
-    <div>
-      <span class="text-gray-800">Hoodie</span>
-      <br>
-      <span class="text-gray-500">Rp.250.000</span>
-    </div>
-    <div
-      class="bg-gray-800 flex gap-2 justify-center items-center rounded-md shadow-md hover:shadow-lg text-gray-100 text-center px-3 py-1">
-      <div>
-        <i class="fas fa-fw fa-cart-plus"></i>
-      </div>
-      <div class="text-xs">
-        Tambah ke keranjang
-      </div>
-    </div>
-  </div>
+  </template>
 
 </div>
+
+<script>
+  const gbody = document.getElementById('grid-body');
+  const temp = document.getElementById('product-card');
+  function formatRupiah(angka) {
+    return new Intl.NumberFormat('id-ID', {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0
+    }).format(angka).replace(/^/, "Rp.");
+  }
+  (async () => {
+    try {
+      const res = await fetch("<?= base("/public/api/produk.php") ?>", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" }
+      })
+      const result = await res.json();
+      if (result.success) {
+        result.data.forEach((row) => {
+          let clone = temp.content.cloneNode(true);
+          clone.querySelector("img").src = `<?= base('/public/uploads/') ?>${row.gambar}`;
+          clone.querySelector("img").alt = row.nama_produk;
+          clone.querySelector(".nama").textContent = row.nama_produk;
+          clone.querySelector(".harga").textContent = formatRupiah(row.harga);
+
+          gbody.appendChild(clone);
+        })
+      }
+    } catch (err) {
+
+    }
+  })()
+</script>
--- public/src/partials/footer.php
+++ public/src/partials/footer.php
@@ -1,11 +1,4 @@
 </div>
-
-<div class="w-full px-5 py-2">
-  <div class="bg-white shadow-md border py-2 px-5 text-center font-medium text-gray-400 text-xs">
-    &copy; 2025 DM Store. Semua Hak Dilindungi.
-  </div>
-</div>
-
 </div>
 
 <script>
